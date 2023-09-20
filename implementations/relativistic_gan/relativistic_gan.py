ad525d86d482b3f338c7a0978fecf5f6de12e6b0
xianghao2023 <xianghao@isrc.iscas.ac.cn>
2023-09-20 15:58:49 +0800

    新增RelativisticGAN

--- implementations/relativistic_gan/relativistic_gan.py
+++ implementations/relativistic_gan/relativistic_gan.py
@@ -1,4 +1,4 @@
-"""SoftmaxGAN Model"""
+"""RelativisticGAN Model"""
 
 import argparse
 import gzip
@@ -17,24 +17,6 @@ from img_utils import to_image
 
 file_path = "../../data/MNIST/"
 
-if not os.path.exists(file_path):
-    # 下载数据集
-    if not os.path.exists('../../data'):
-        os.mkdir('../../data')
-    os.mkdir(file_path)
-    base_url = 'http://yann.lecun.com/exdb/mnist/'
-    file_names = ['train-images-idx3-ubyte.gz', 'train-labels-idx1-ubyte.gz',
-                  't10k-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz']
-    for file_name in file_names:
-        url = (base_url + file_name).format(**locals())
-        print("Downloading MNIST dataset from" + url)
-        urllib.request.urlretrieve(url, os.path.join(file_path, file_name))
-        with gzip.open(os.path.join(file_path, file_name), 'rb') as f_in:
-            print("Unzipping...")
-            with open(os.path.join(file_path, file_name)[:-3], 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-        os.remove(os.path.join(file_path, file_name))
-
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
@@ -45,13 +27,12 @@ parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first 
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
+parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument("--rel_avg_gan", action="store_true", help="relativistic average GAN instead of standard")
 opt = parser.parse_args()
 print(opt)
-
-img_shape = (opt.channels, opt.img_size, opt.img_size)
 
 
 class Generator(nn.Cell):
@@ -60,25 +41,30 @@ class Generator(nn.Cell):
     def __init__(self):
         super().__init__(Generator)
 
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Dense(in_feat, out_feat)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8,affine=False))
-            layers.append(nn.LeakyReLU(0.2))
-            return layers
+        self.init_size = opt.img_size // 4
+        self.l1 = nn.SequentialCell(nn.Dense(opt.latent_dim, 128 * self.init_size ** 2))
 
-        self.model = nn.SequentialCell(
-            *block(opt.latent_dim, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Dense(1024, int(np.prod(img_shape))),
+        self.conv_blocks = nn.SequentialCell(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2.0, recompute_scale_factor=True),
+            nn.Conv2d(128, 128, 3, stride=1,
+                      pad_mode='pad', padding=1),
+            nn.BatchNorm2d(128, 0.8, affine=False),
+            nn.LeakyReLU(0.2),
+            nn.Upsample(scale_factor=2.0, recompute_scale_factor=True),
+            nn.Conv2d(128, 64, 3, stride=1,
+                      pad_mode='pad', padding=1),
+            nn.BatchNorm2d(64, 0.8, affine=False),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(64, opt.channels, 3, stride=1,
+                      pad_mode='pad', padding=1),
             nn.Tanh()
         )
 
     def construct(self, _z):
-        img = self.model(_z)
-        img = img.view(img.shape[0], *img_shape)
+        out = self.l1(_z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
         return img
 
 
@@ -88,23 +74,38 @@ class Discriminator(nn.Cell):
     def __init__(self):
         super().__init__(Discriminator)
 
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [
+                nn.Conv2d(in_filters, out_filters, 3, 2,
+                          'pad', 1),
+                nn.LeakyReLU(0.2),
+                nn.Dropout2d(0.25)
+            ]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8, affine=False))
+            return block
+
         self.model = nn.SequentialCell(
-            nn.Dense(opt.img_size ** 2, 512),
-            nn.LeakyReLU(0.2),
-            nn.Dense(512, 256),
-            nn.LeakyReLU(0.2),
-            nn.Dense(256, 1),
+            *discriminator_block(opt.channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
         )
 
+        # The height and width of downsampled image
+        ds_size = opt.img_size // 2 ** 4
+        self.adv_layer = nn.SequentialCell(nn.Dense(128 * ds_size ** 2, 1))
+
     def construct(self, _img):
-        img_flat = _img.view(_img.shape[0], -1)
-        validity = self.model(img_flat)
+        out = self.model(_img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
 
         return validity
 
 
-# Loss functions
-adversarial_loss = nn.BCELoss()
+# Loss function
+adversarial_loss = nn.BCEWithLogitsLoss()
 
 # Initialize generator and discriminator
 generator = Generator()
@@ -127,46 +128,47 @@ optimizer_G = nn.optim.Adam(generator.trainable_params(), learning_rate=opt.lr, 
 optimizer_D = nn.optim.Adam(discriminator.trainable_params(), learning_rate=opt.lr, beta1=opt.b1, beta2=opt.b2)
 
 
-def log(x):
-    """Log function"""
-    return ops.log(x + 1e-8)
-
-
-def g_forward(_real_imgs, _g_target):
+def g_forward(_real_imgs, _valid):
     """Generator forward function"""
     # Generate a batch of images
     z = ops.randn((_real_imgs.shape[0], opt.latent_dim), dtype=mstype.float32)
     _gen_imgs = generator(z)
 
-    _d_real = discriminator(_real_imgs)
-    _d_fake = discriminator(_gen_imgs)
+    real_pred = ops.stop_gradient(discriminator(_real_imgs))
+    fake_pred = discriminator(_gen_imgs)
 
-    # Partition function
-    Z = ops.sum(ops.exp(-_d_real)) + ops.sum(ops.exp(-_d_fake))
+    if opt.rel_avg_gan:
+        _g_loss = adversarial_loss(fake_pred - real_pred.mean(0, keep_dims=True), _valid)
+    else:
+        _g_loss = adversarial_loss(fake_pred - real_pred, _valid)
 
-    # Calculate loss of generator and update
-    _g_loss = _g_target * (ops.sum(_d_real) + ops.sum(_d_fake)) + log(Z)
+    # Loss measures generator's ability to fool the discriminator
+    _g_loss = adversarial_loss(discriminator(_gen_imgs), _valid)
 
     return _g_loss, _gen_imgs
 
 
-def d_forward(_real_imgs, _gen_imgs, _d_target):
+def d_forward(_real_imgs, _gen_imgs, _valid, _fake):
     """Discriminator forward function"""
 
-    _d_real = discriminator(_real_imgs)
-    _d_fake = discriminator(_gen_imgs)
+    # Predict validity
+    real_pred = discriminator(_real_imgs)
+    fake_pred = discriminator(_gen_imgs)
 
-    # Partition function
-    Z = ops.sum(ops.exp(-_d_real)) + ops.sum(ops.exp(-_d_fake))
+    if opt.rel_avg_gan:
+        real_loss = adversarial_loss(real_pred - fake_pred.mean(0, keepdim=True), _valid)
+        fake_loss = adversarial_loss(fake_pred - real_pred.mean(0, keepdim=True), _fake)
+    else:
+        real_loss = adversarial_loss(real_pred - fake_pred, _valid)
+        fake_loss = adversarial_loss(fake_pred - real_pred, _fake)
 
-    # Calculate loss of discriminator and update
-    _d_loss = _d_target * ops.sum(_d_real) + log(Z)
+    _d_loss = (real_loss + fake_loss) / 2
 
-    return _d_loss, _d_real, _d_fake
+    return _d_loss
 
 
 grad_g = ops.value_and_grad(g_forward, None, optimizer_G.parameters, has_aux=True)
-grad_d = ops.value_and_grad(d_forward, None, optimizer_D.parameters, has_aux=True)
+grad_d = ops.value_and_grad(d_forward, None, optimizer_D.parameters, has_aux=False)
 
 # ----------
 #  Training
@@ -178,29 +180,25 @@ discriminator.set_train()
 for epoch in range(opt.n_epochs):
     for i, (imgs, _) in enumerate(dataset.create_tuple_iterator()):
 
-        batch_size = imgs.shape[0]
-
         # Adversarial ground truths
-        g_target = 1 / (batch_size * 2)
-        d_target = 1 / batch_size
+        valid = ops.stop_gradient(ops.ones((imgs.shape[0], 1)))
+        fake = ops.stop_gradient(ops.zeros((imgs.shape[0], 1)))
 
         # Configure input
         real_imgs = imgs
-
 
         # -----------------
         #  Train Generator
         # -----------------
 
-        (g_loss, gen_imgs), g_grads = grad_g(real_imgs, g_target)
+        (g_loss, gen_imgs), g_grads = grad_g(real_imgs, valid)
         optimizer_G(g_grads)
 
         # ---------------------
         #  Train Discriminator
         # ---------------------
 
-        (d_loss, d_real, d_fake), d_grads = grad_d(real_imgs,
-                                                   ops.stop_gradient(gen_imgs), d_target)
+        (d_loss), d_grads = grad_d(real_imgs, ops.stop_gradient(gen_imgs), valid, fake)
         optimizer_D(d_grads)
 
         # --------------
